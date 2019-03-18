320b26ee677d1403c74f11fff6c3f8503594de97
mereysdu <yerkeboolan@gmail.com>
2019-03-19 03:44:38 +0600

    editing home && contact pages...

--- ng-project/src/app/app.module.ts
+++ ng-project/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ReactiveFormsModule } from '@angular/forms';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { OwlModule } from 'ngx-owl-carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +25,9 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SlickCarouselModule,
+    OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng-project/src/app/home/home.component.ts
+++ ng-project/src/app/home/home.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  title = 'OwlCarousel2 in Angular7 with Custom Navigation Arrows';
+ 
+  carouselOptions = {
+    margin: 25,
+    mouseDrag: false,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1500,
+    dotsSpeed: 300,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+    }
+  }
+ 
+  images = [
+    {
+      image: "./assets/People-Sport.jpg"
+    },
+    {
+      image: "./assets/first_photo.jpg"
+    },
+    {
+     
+      image: "./assets/second_photo.jpg"
+    },
+    {
+      image: "./assets/third_photo.jpg"
+    },   
+  ]
 
   constructor() { }
 

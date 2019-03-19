24f0eb1e3300d4db51f77b9a1f521cdf3e912ad7
mereysdu <yerkeboolan@gmail.com>
2019-03-19 18:52:10 +0600

    home page is edited

--- ng-project/src/app/carousel/carousel.component.ts
+++ ng-project/src/app/carousel/carousel.component.ts
@@ -6,6 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
+  title = 'OwlCarousel2 in Angular7 with Custom Navigation Arrows';
+ 
+  carouselOptions = {
+    margin: 25,
+    mouseDrag: false,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    responsiveClass: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    smartSpeed: 1500,
+    dotsSpeed: 300,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false
+      },
+    }
+  }
+ 
+  images = [
+    {
+      image: "./assets/0.jpg"
+    },
+    {
+      image: "./assets/1.jpg"
+    },
+    {
+     
+      image: "./assets/2.jpg"
+    },
+    {
+      image: "./assets/3.jpg"
+    },   
+  ]
 
   constructor() { }
 
--- ng-project/src/app/comment/comment.component.ts
+++ ng-project/src/app/comment/comment.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-  commentAuthor1: string = "Rinat Sabitov"
-  commentAuthor2: string = "Maden Dinara"
+  
   constructor() { }
 
   ngOnInit() {
